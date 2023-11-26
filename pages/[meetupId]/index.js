bf0e9841b5ccb3dc11cbba1c887f25671e8f2848
Wasim Hyder <wasim@creativefaze.com>
2023-11-26 18:18:31 +0500

    Added detail page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,11 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+function meetupDetails() {
+  return <MeetupDetail
+    img="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+    title="First meetup"
+    address="Some address"
+    description="This is first meetup"
+  />
+}
+export default meetupDetails;
